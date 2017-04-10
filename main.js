8239bb3fd3ffad8c5d1d3ee0b1494f532827a53a
Zao Soula <zaonono@hotmail.fr>
2017-04-10 19:54:23 +0200

    V1.0.1

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ var app = express();
 
 var routes = require("./routes/routes.js")(app);
 
-var server = app.listen(80, function() {
+var server = app.listen(3000, function() {
     console.log("Listening on port %s...", server.address().port);
 });
